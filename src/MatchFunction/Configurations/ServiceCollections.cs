0a86a4364267c13d46fc93cce45e8f9d0fcca38c
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2023-10-17 17:00:58 -0400

    chore: cleanup

--- src/MatchFunction/Configurations/ServiceCollections.cs
+++ src/MatchFunction/Configurations/ServiceCollections.cs
@@ -1,29 +1,24 @@
 ﻿namespace MatchFunction.Configurations;
 
-public static class ServiceCollections
-{
-    public static IServiceCollection AddGrpcService(this IServiceCollection services)
-    {
+public static class ServiceCollections {
+
+    public static IServiceCollection AddGrpcService(this IServiceCollection services) {
         services.AddGrpc().AddJsonTranscoding();
         services.AddGrpcReflection();
 
         return services;
     }
 
-    public static IServiceCollection AddHealthChecksService(this IServiceCollection services)
-    {
+    public static IServiceCollection AddHealthChecksService(this IServiceCollection services) {
         services.AddGrpcHealthChecks()
             .AddCheck("Sample", () => HealthCheckResult.Healthy());
 
         return services;
     }
 
-    public static IServiceCollection AddSwaggerService(this IServiceCollection services)
-    {
-        
+    public static IServiceCollection AddSwaggerService(this IServiceCollection services) {
         services.AddGrpcSwagger();
-        services.AddSwaggerGen(c =>
-        {
+        services.AddSwaggerGen(c => {
             c.SwaggerDoc("v1",
                 new OpenApiInfo { 
                     Version = "v1",
